1cd556d8390f5433076feef4fd42b5bcd27b86c0
joaopedromatias <joaopedromatias@outlook.com>
2024-01-31 21:23:56 -0300

    rename var

--- merge_sort.py
+++ merge_sort.py
@@ -1,8 +1,8 @@
-def merge_sort(unsorted_array):
-    if (len(unsorted_array) > 1):
-        index_to_divide = len(unsorted_array) // 2
-        left = unsorted_array[:index_to_divide]
-        right = unsorted_array[index_to_divide:]
+def merge_sort(array):
+    if (len(array) > 1):
+        index_to_divide = len(array) // 2
+        left = array[:index_to_divide]
+        right = array[index_to_divide:]
 
         merge_sort(left)
         merge_sort(right)
@@ -11,20 +11,20 @@ def merge_sort(unsorted_array):
         right_elements_index_to_look_for = 0 
         original_arr_modification_index = 0
 
-        for i in range(len(unsorted_array)): 
+        for i in range(len(array)): 
             if left_elements_index_to_look_for < len(left) and right_elements_index_to_look_for < len(right): 
                 if(left[left_elements_index_to_look_for] < right[right_elements_index_to_look_for]):
-                    unsorted_array[i] = left[left_elements_index_to_look_for]
+                    array[i] = left[left_elements_index_to_look_for]
                     left_elements_index_to_look_for += 1
                 else: 
-                    unsorted_array[i] = right[right_elements_index_to_look_for]
+                    array[i] = right[right_elements_index_to_look_for]
                     right_elements_index_to_look_for += 1
                 original_arr_modification_index += 1
             elif left_elements_index_to_look_for == len(left):
-                unsorted_array[i] = right[right_elements_index_to_look_for]
+                array[i] = right[right_elements_index_to_look_for]
                 right_elements_index_to_look_for += 1
             else: 
-                unsorted_array[i] = left[left_elements_index_to_look_for]
+                array[i] = left[left_elements_index_to_look_for]
                 left_elements_index_to_look_for += 1
         
-        return unsorted_array
+        return array
