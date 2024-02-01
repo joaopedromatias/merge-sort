de1f3f0cbaaa46ad773693a01f0a83833954ba3d
joaopedromatias <joaopedromatias@outlook.com>
2024-01-31 21:17:22 -0300

    remove call

--- merge_sort.py
+++ merge_sort.py
@@ -28,7 +28,3 @@ def merge_sort(unsorted_array):
                 left_elements_index_to_look_for += 1
         
         return unsorted_array
-
-
-print(merge_sort([101,100,3,1,1,8,2,5,9,9,10,100,7,-9,39]))
-# [1,3,8,2,5,9,10,7]
